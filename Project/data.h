12392bacf9fc7321ba9148f6a54902511a111445
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2022-06-02 19:42:18 +0200

    changes in hillclimbing

--- Project/data.h
+++ Project/data.h
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-int howManyBin(std::vector<int> data, int binSize, int quantity);
-void hillClimbingRandom(std::vector<int> data, int binSize, int quantity, int iterations);
-void hillClimbing(std::vector<int> data, int binSize, int quantity, int iterations);
-void tabuSearch(std::vector<int> data, int binSize, int quantity, int tabuSize, int iterations);
+int howManyBin(vector<int> data, int binSize, int quantity);
+void hillClimbingRandom(vector<int> data, int binSize, int quantity, int iterations);
+void hillClimbing(vector<int> data, int binSize, int quantity, int iterations);
+void tabuSearch(vector<int> data, int binSize, int quantity, int tabuSize, int iterations);
 void simulatedAnnealing(vector<int> solution, int binSize, int quantity, int iterations, bool uniformRealDistributionIsSet);
 
 #endif //BINPACKINGPROBLEM_DATA_H
--- Project/hillClimbing.cpp
+++ Project/hillClimbing.cpp
@@ -5,37 +5,44 @@
 #include <ctime>
 
 using namespace std;
-void hillClimbing(vector<int> solution, int binSize, int quantity, int iterations) {
+void hillClimbing(vector<int> data, int binSize, int quantity, int iterations) {
 
-    int amountOfBins = howManyBin(solution, binSize, quantity);
+    int amountOfBins = howManyBin(data, binSize, quantity);
     int lowestAmountOfBins = amountOfBins;
+    int iteration_counter = 0;
 
     srand((unsigned) time(NULL));
 
-    for (int i = 0; i < iterations; ++i) {
-        int x = rand() % (end(solution) - begin(solution));
-        for (int j = 0; j < solution.size(); ++j) {
-            swap(solution[x], solution[j]);
-            amountOfBins = howManyBin(solution, binSize, quantity);
+    for (iteration_counter = 0; iteration_counter < iterations; ++iteration_counter) {
+        int x = rand() % (end(data) - begin(data));
+        for (int j = 0; j < data.size(); ++j) {
+            swap(data[x], data[j]);
+            amountOfBins = howManyBin(data, binSize, quantity);
             if (amountOfBins < lowestAmountOfBins) {
                 lowestAmountOfBins = amountOfBins;
             }
         }
-        cout  << "Amounts of bins in this run: " << amountOfBins << endl << "Amounts of bins in best run: "
-        << lowestAmountOfBins << endl << "Iterations: " << i << endl;
+
     }
+    cout << "Amounts of bins in best run: " << lowestAmountOfBins << endl << "Iterations: " << iteration_counter << endl;
+     for(auto i: data){
+        cout << i << ", ";
+     }
 }
 
 void hillClimbingRandom(vector<int> solution, int binSize, int quantity, int iterations) {
     int amountOfBins, lowestAmountOfBins = howManyBin(solution, binSize, quantity);
+    int iteration_counter = 0;
 
-    for (int i = 0; i < iterations; ++i) {
+    for (iteration_counter = 0; iteration_counter < iterations; ++iteration_counter) {
         shuffle(begin(solution), end(solution), mt19937(random_device()()));
         amountOfBins = howManyBin(solution, binSize, quantity);
         if (amountOfBins < lowestAmountOfBins) {
             lowestAmountOfBins = amountOfBins;
         }
-        cout  << "Amounts of bins in this run: " << amountOfBins << endl << "Amounts of bins in best run: "
-        << lowestAmountOfBins << endl << "Iterations: " << i << endl;
+    }
+    cout << "Amounts of bins in best run: " << lowestAmountOfBins << endl << "Iterations: " << iteration_counter << endl;
+    for(auto i: solution){
+        cout << i << ", ";
     }
 }
