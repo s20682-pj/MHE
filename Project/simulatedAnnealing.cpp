823150e120d2b7d0de8eb3e7f932523b5994986b
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2022-06-03 20:19:29 +0200

    Update simulatedAnnealing.cpp

--- Project/simulatedAnnealing.cpp
+++ Project/simulatedAnnealing.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 vector<int> simulatedAnnealing(vector<int> solution, int binSize, int quantity, int iterations, bool uniformRealDistributionIsSet) {
 
-    int result, nextResult, bestResult;
+    int result = howManyBin(solution, binSize, quantity);
+    int nextResult, bestResult;
     double boltzmannDistribution;
     vector<int> bestSolution = solution;
     vector<int> nextSolution = solution;
@@ -19,15 +20,16 @@ vector<int> simulatedAnnealing(vector<int> solution, int binSize, int quantity, 
 
     for (iteration_counter = 0; iteration_counter < iterations; ++iteration_counter) {
         shuffle(begin(nextSolution), end(nextSolution), gen);
-        result = howManyBin(solution, binSize, quantity);
         nextResult = howManyBin(nextSolution, binSize, quantity);
         boltzmannDistribution = exp(-abs(result - nextResult) / (iterations/(iteration_counter+1)));
         if ( nextResult < result ){
             solution = nextSolution;
             result = nextResult;
+            //rozkład liniowy
         } else if (uniformRealDistributionIsSet && (uniformRealDistribution(gen) < boltzmannDistribution)) {
             solution = nextSolution;
             result = nextResult;
+            //rozkład normalny
         } else if (!uniformRealDistributionIsSet && (normalDistribution(gen) < boltzmannDistribution)) {
             solution = nextSolution;
             result = nextResult;
